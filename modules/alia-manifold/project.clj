58c005c78bae54f64e07eda513fb022ab988ac2f
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-01-04 11:12:56 +0000

    private version of alia-manifold

--- modules/alia-manifold/project.clj
+++ modules/alia-manifold/project.clj
@@ -1,4 +1,6 @@
-(defproject cc.qbits/alia-manifold alia-version
+(def alia-version "4.0.0-beta3")
+(def clj-version "1.9.0-alpha14")
+(defproject mccraigmccraig/alia-manifold alia-version
   :url "https://github.com/mpenet/alia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
